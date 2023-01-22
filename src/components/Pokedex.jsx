329dac74f5863cf4c4389d05967f45c747b5220c
CarolinaVlr <dianacarolinav97@gmail.com>
2023-01-22 11:09:21 -0500

    seVuelveASubir

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -3,26 +3,42 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import trainerName from '../store/slice/trainerName.slice'
 import PokeCard from './PokeCard';
 
 const Pokedex = () => {
     const [pokemon, setPokemon] = useState([])
     const [names, setNames] = useState("")
+    const [pokemonType, setPokemonType] = useState([])
+    
+    const navigate = useNavigate()
     const name = useSelector((state)=> state.trainerName)
     useEffect(()=>{
         axios.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10")
         .then(res=> setPokemon(res.data.results))
+
+        axios.get('https://pokeapi.co/api/v2/type/')
+        .then(res => setPokemonType(res.data.results))
         
             
     },[])
 
-    const refresh = ()=>{
-        
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${names}/`)
-        .then(res => setSearchPokemon(res.data))
+    const searchName = ()=>{
+        navigate(`/pokeCard/%${names}`)
 
     }
+
+    const searchLocation = (url) =>{
+        axios.get(url).then.res((res)=>{setPokemon(res.data)})
+    }
+
+    const [page, setPage] = useState(1)
+    const pokemonForPage = 9
+    const lastPokemon = page * pokemonForPage
+    const firstPokemon = lastPokemon - pokemonForPage
+    const pokemonPagined = setPokemon.slice(firstPokemon,lastPokemon)
+    const totalpages = 
 
     return (
         <div>
